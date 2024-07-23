a8b96ebd4f4d1da7f948d93aa96f0a29a9a52eab
Felix Li <04felix.li@gmail.com>
2024-07-23 11:50:42 -0400

    Fix sidebar UI

--- app/global components/Navigation/SideBar.tsx
+++ app/global components/Navigation/SideBar.tsx
@@ -6,6 +6,7 @@ import { FaFontAwesomeLogoFull } from "react-icons/fa";
 import { LogoutLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
 import { usePathname } from 'next/navigation';
 import { useSideBarContext } from '../../contexts/sideBarContext';
+import { CiLogout } from "react-icons/ci";
 
 interface Props {
     children: ReactNode;
@@ -19,34 +20,55 @@ const SideBar = ({ children }: Props) => {
         <aside className='fixed top-0 left-0 h-full'>
             {isAuthenticated && <>
                 <nav className='h-full flex flex-col bg-black rounded-r-md shadow-sm p-4'>
-
-                    <div className='pb-2 flex items-center justify-center'>
-                        <FaFontAwesomeLogoFull className={`overflow-hidden transition-all ${expanded ? 'w-52' : 'w-0'}`} />
-                        <button onClick={() => setExpanded(prevState => !prevState)}>
-                            <BsLayoutTextSidebarReverse
-                                className={`h-5 w-5 flex-shrink-0 bg-gray-500 hover:bg-gray-600 hover:scale-105 duration-300`}
-                            />
-                        </button>
-                    </div>
-
-                    <ul className='flex-1'>{children}</ul>
-
-                    {isAuthenticated &&
-                        <LogoutLink className='border rounded-md px-3 flex items-center justify-center py-2 mb-3'>
-                            Log Out
-                        </LogoutLink>
-                    }
-
-                    <div className='border-t flex p-3 items-center justify-center'>
-                        <CgProfile className='w-7 h-7 flex-shrink-0' />
-                        <div className={`flex justify-between items-center overflow-hidden transition-all ${expanded ? 'w-52 ml-3' : 'w-0 h-0'}`}>
-                            <div className='leading-4'>
-                                <h4 className='font-semibold'>Test User</h4>
-                                <span className='text-xs'>testUser@gmail.com</span>
-                            </div>
+                    <div className={`rounded-md w-full my-2 px-3`}>
+                        <div className={`relative px-3 py-3 flex items-center font-medium rounded-md transition-colors group`}>
+                            <span className={`overflow-hidden transition-all font-bold text-xl ${expanded ? "w-52 ml-3" : "w-0"}`}>SlimPossible</span>
+                            <button onClick={() => setExpanded(prevState => !prevState)}>
+                                <BsLayoutTextSidebarReverse
+                                    className={`h-5 w-5 flex-shrink-0 cursor-pointer bg-gray-500 hover:bg-gray-600 hover:scale-105 duration-300`}
+                                />
+                            </button>
                         </div>
                     </div>
 
+                    <ul className={`flex-1 px-3`}>{children}</ul>
+
+                    {/* {isAuthenticated &&
+                        <LogoutLink className='border rounded-md px-3 flex items-center justify-center py-2 mb-3 hover:scale-105 hover:bg-card-bg-gradient-light hover:duration-300'>
+                            Log Out
+                        </LogoutLink>
+                    } */}
+
+                    <div className={`rounded-md w-full my-2 px-3 hover:scale-105 hover:bg-card-bg-gradient-light hover:duration-300`}>
+                        <div className={`relative px-3 py-3 flex items-center font-medium rounded-md transition-colors group`}>
+                            <span className={`overflow-hidden transition-all text-nowrap ${expanded ? "w-52 ml-3" : "w-0"}`}>Log out</span>
+                            <CiLogout
+                                className={`h-5 w-5 flex-shrink-0 cursor-pointer`}
+                            />
+                            {!expanded && (
+                                <div
+                                    className={`absolute text-nowrap left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-2`}
+                                >
+                                    Log out
+                                </div>
+                            )}
+                        </div>
+                    </div>
+
+
+                    <div className={`w-full my-2 px-3 border-t`}>
+                        <div className={`relative px-3 py-3 flex items-center font-medium rounded-md transition-colors group`}>
+                            <CgProfile
+                                className={`h-5 w-5 flex-shrink-0`}
+                            />
+                            <div className={`flex justify-between items-center overflow-hidden transition-all ${expanded ? 'w-52 ml-3' : 'w-0'}`}>
+                                <div className='leading-4'>
+                                    <h4 className='font-semibold'>Test User</h4>
+                                    <span className='text-xs'>testUser@gmail.com</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </nav>
             </>}
         </aside >
--- app/global components/Navigation/SideBarItem.tsx
+++ app/global components/Navigation/SideBarItem.tsx
@@ -1,8 +1,8 @@
-"use client"
-import React, { ReactNode, useContext } from 'react'
-import Link from 'next/link';
-import { useSideBarContext } from '../../contexts/sideBarContext';
-import { usePathname } from 'next/navigation';
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useSideBarContext } from "../../contexts/sideBarContext";
+import { usePathname } from "next/navigation";
 
 interface Props {
     page: string;
@@ -15,13 +15,26 @@ const SideBarItem = ({ page, icon, text }: Props) => {
     const pathname = usePathname();
 
     return (
-        <Link href={page} className={`relative flex items-center justify-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group ${pathname == page ? 'border' : 'hover:scale-105 duration-300'}`}>
-            {icon}
-            <span className={`overflow-hidden transition-all ${expanded ? 'w-52 ml-3' : 'w-0 h-0'}`}>{text}</span>
+        <div
+            className={`rounded-md w-full my-2 ${pathname == page ? "bg-darkest-color text-disabled-color" : "hover:scale-105 hover:bg-card-bg-gradient-light hover:duration-300"}`}
+        >
+            <Link
+                href={page}
+                className={`relative px-3 py-3 flex items-center font-medium rounded-md cursor-pointer transition-colors group`}
+            >
+                {icon}
+                <span className={`overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}`}>{text}</span>
 
-            {!expanded && <div className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}>{text}</div>}
-        </Link>
-    )
-}
+                {!expanded && (
+                    <div
+                        className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-2`}
+                    >
+                        {text}
+                    </div>
+                )}
+            </Link>
+        </div>
+    );
+};
 
-export default SideBarItem
+export default SideBarItem;
